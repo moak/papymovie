6babd55adc8af56392467f90ee40280fb2d06d6d
Maxime <barbot.max@gmail.com>
2020-11-16 22:46:20 +0100

    ✅ [ADD] (core) Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,22 +1,26 @@
 const { nextI18NextRewrites } = require('next-i18next/rewrites');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const localeSubpaths = {
   fr: 'fr',
   en: 'en',
 };
 
-console.log({
+module.exports = withBundleAnalyzer({
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GITHUB_ID: process.env.GITHUB_ID,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
     THEMOVIEDB_API_KEY: process.env.THEMOVIEDB_API_KEY,
     MONGO_URI: process.env.MONGO_URI,
+    MONGODB_DB: process.env.MONGODB_DB,
 
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
@@ -30,29 +34,3 @@ console.log({
     GA_TRACKING_ID: process.env.GA_TRACKING_ID,
   },
 });
-
-module.exports = {
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    THEMOVIEDB_API_KEY: process.env.THEMOVIEDB_API_KEY,
-    MONGO_URI: process.env.MONGO_URI,
-
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_REDIRECT: process.env.AUTH0_REDIRECT,
-    AUTH0_POST_LOGOUT_REDIRECT: process.env.AUTH0_POST_LOGOUT_REDIRECT,
-    AUTH0_COOKIE_SECRET: process.env.AUTH0_COOKIE_SECRET,
-
-    FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
-
-    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
-  },
-};
